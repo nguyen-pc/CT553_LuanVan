7080e5516e9fd8eb8900134d67622b72b757d401
nguyen <nguyenthcs430@gmail.com>
2025-08-20 20:47:53 +0700

    init project

---
+++ src/main/java/com/vn/petcare/PlatformPetCareApplication.java
@@ -0,0 +1,13 @@
+package com.vn.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformPetCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformPetCareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vn/petcare/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.vn.petcare.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test successful: ");
+    }
+}
